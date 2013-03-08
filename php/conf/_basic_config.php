2a9b0ae9614664a6528f0c6a1f81a1f606118286
Jose R Rodriguez <jose@metztli-it.com>
2013-03-08 05:49:53 -0800

    Enhanced tunning of b_config to accomodate some values

--- php/conf/_basic_config.php
+++ php/conf/_basic_config.php
@@ -104,11 +104,11 @@ $allow_evodb_reset = 0;	// Set to 1 to enable. Do not leave this on 1 on product
 /* Jose R Rodriguez/Metztli IT( http://www.metztli-it.com ) 03-08-2013 */
 /* $baseurl = 'http://AppName-uniqueNameSpace.rhcloud.com/'; */
 // Use the following if you want to use the current domain:
-if( isset($_SERVER['HTTP_HOST']) 
-{	// This only works if HOSt provided by webserver (i-e DOES NOT WORK IN PHP CLI MODE)
+/* if( isset($_SERVER['HTTP_HOST']) 
+{*/	// This only works if HOSt provided by webserver (i-e DOES NOT WORK IN PHP CLI MODE)
 	$baseurl = ( (isset($_SERVER['HTTPS']) && ( $_SERVER['HTTPS'] != 'off' ) ) ?'https://':'http://')
 							.$_SERVER['HTTP_HOST'].'/';
-}
+/*}*/
 /**
  * This is used only to create the Admin account.
  * @todo move to installer.
